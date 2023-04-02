3c38358d9bdcb48e5c6679cc5585030de83e9c80
Oleksii Maievskyi <oleksii.maievskyi@dxc.com>
2023-04-02 16:22:59 +0300

    Basket microservices

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿namespace Basket.API.Entities
+{
+	public class ShoppingCart
+	{
+        public ShoppingCart()
+        {
+            
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+            Items = new List<ShoppingCartItem>();
+        }
+        public string UserName { get; set; }
+		public List<ShoppingCartItem> Items { get; set; }
+
+        public decimal TotalPrice
+        {
+            get 
+            { 
+                decimal total = 0;
+                foreach (var item in Items)
+                {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+
+        }
+
+    }
+}
